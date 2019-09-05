54809380aec7b2efc0caf6fc48bfaf68c645a42f
ErikaLFrost <erika.loman@gmail.com>
2019-09-05 13:25:50 +0200

    fixat med uls

---
+++ assets/js/disc.js
@@ -0,0 +1,14 @@
+let app = {};
+
+function showDiscFunc(){
+    
+
+}
+
+app.selectElementSixt = document.getElementsByid("disch2-sixt-sevent");
+app.selectElementEighti = document.getElementsByid("disch2-eighti-nineti");
+app.selectElementMillen = document.getElementsByid("disch2-millen-list");
+
+app.selectElementSixt.addEventListenerFunction("click", showDiscFunc)
+app.selectElementEight.addEventListenerFunction("click", showDiscFunc)
+app.selectElementMillen.addEventListenerFunction("click", showDiscFunc)
